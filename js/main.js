36d31ada139bb960edccf46e16e2a946e2d028a3
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-02-23 17:00:23 +0100

    Bonus 1 e 2

--- js/main.js
+++ js/main.js
@@ -1,20 +1,34 @@
 'use strict'
 
+const containerFizzBuzz = document.getElementById('container-fizzbuzz');
+
 for (let i = 1; i <= 100; i++) {
+
+    const stringFizzBuzz = document.createElement('div');
+    containerFizzBuzz.append(stringFizzBuzz);
+    stringFizzBuzz.classList.add('box');
 
     if (i % 3 === 0 && i % 5 !== 0) {
         console.log('Fizz')
+        stringFizzBuzz.append('Fizz');
+        stringFizzBuzz.classList.add('box-fizz');
     }
 
     else if (i % 5 === 0 && i % 3 !== 0) {
         console.log('Buzz')
+        stringFizzBuzz.append('Buzz');
+        stringFizzBuzz.classList.add('box-buzz');
     }
 
     else if (i % 3 === 0 && i % 5 === 0) {
         console.log('FizzBuzz')
+        stringFizzBuzz.append('FizzBuzz');
+        stringFizzBuzz.classList.add('box-fizzbuzz');
     }
 
     else {
         console.log(i);
+        stringFizzBuzz.append(i);
+        stringFizzBuzz.classList.add('box-number');
     }
 }
